623a44eb591f7329ef568636c38237754a6dc124
Simon <simon@ferdiesfoodlab.co.uk>
2019-03-09 10:39:28 +0000

    Access mySQL DB - list.py=ver.script hello.py=ver.flask

--- list.py
+++ list.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#! /usr/bin/env python
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -7,35 +6,46 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 # giza a look
 from pprint import pprint
 
-import urllib.parse
+import urllib.parse                     # sed to parse passwords into url format
 url_encoded_pwd = urllib.parse.quote_plus("kx%jj5/g")
 
 
 # default
 #engine = db.create_engine('dialect+driver://user:pass@host:port/db')
+#engine = create_engine('mysql://scott:tiger@localhost/foo')
+
+#engine = create_engine('mysql://root:@localhost/nutridb_sr25_sanitized')
+#engine = create_engine('mysql://root:meepmeep@localhost/nutridb_sr25_sanitized')
+
+# to fine user and port at msql prompt
+# mysql> select user();
+# mysql> show variables;
 engine = create_engine('mysql://root:meepmeep@localhost:3306/nutridb_sr25_sanitized')
+
 db = scoped_session(sessionmaker(bind=engine))
 
+pprint(engine)
 
-# each app.route is an endpoint
-@app.route('/')
-def hello_world():
+# If there are many levels of recursion, and you don't want to see them all
+# you can use the depth parameter to limit how many levels it goes down
+#pprint(engine, depth=2)
+
+
+
+def main():
     # execute this query
     # SELECT ndb_no, nutr_no, nutr_val, deriv_cd FROM nutrientdata ORDER BY nutr_val LIMIT 10;
     db_lines = db.execute("SELECT ndb_no, nutr_no, nutr_val, deriv_cd FROM nutrientdata ORDER BY nutr_val LIMIT 10;").fetchall()
- 
-    formatted_text = "<br>"
+    
+    formatted_text = "\n"
     
     for line in db_lines:
         #print(f" | {line.ndb_no} | {line.nutr_no} | {line.nutr_val} | {line.deriv_cd} | ")
-        formatted_text = formatted_text + f" | {line.ndb_no} | {line.nutr_no} | {line.nutr_val} | {line.deriv_cd} | <br>"
+        formatted_text = formatted_text + f" | {line.ndb_no} | {line.nutr_no} | {line.nutr_val} | {line.deriv_cd} | \n"
     
     print(f"\n\nProcess Query {formatted_text}")
-    
-    return f"Processed Query:<br>{formatted_text} <br>END"
-    
-    
-
+        
+    print(f"\n\nHello World > {engine}" <)
 
 
 if __name__ == '__main__':
