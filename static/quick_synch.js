19feb07de7b7e5f34e73281f31838a572c6b4771
Simon <simon@ferdiesfoodlab.co.uk>
2019-10-30 20:16:42 +0000

    quick_synch tidy

--- static/quick_synch.js
+++ static/quick_synch.js
@@ -4,31 +4,66 @@
 //# render blank html, with simple JS to POST status (THIS javascript)
 //# post user, device, dt_date from dtk in local storage on device
 //# compare to data for user on server
-//# if dt_date on device is before 5AM today (set by user) store the dtk
+//# if dt_date on device is before 5AM today (set by user) arechive the dtk
 
+// fingerprinting code - - - - - < 
+
+// no lib for initial simple id - but give this a go: fingerprintjs2
+// navigator.appName
+// navigator.appVersion
+// navigator.platform
+// navigator.userAgent
+var fingerprint = {}; 
+
+var storeFingerprint = function () {
+  var d1 = new Date();  
+  var fingerprint = {};
+  
+  Fingerprint2.get( function(components) {
+    fingerprint['fp'] = Fingerprint2.x64hash128(components.map(function (pair) { return pair.value }).join(), 31);
+    fingerprint['appName'] = navigator.appName;
+    fingerprint['appVersion'] = navigator.appVersion;
+    fingerprint['platform'] = navigator.platform;
+    fingerprint['userAgent'] = navigator.userAgent;
+  
+    var d2 = new Date();
+    var time = d2 - d1;
+    fingerprint['timeTaken'] = time;
+    console.log(`==> FP: ${fingerprint}`);
+    console.log(fingerprint);
+
+    if (typeof(Storage) !== "undefined") {
+      window.localStorage.setItem( 'fingerprint', JSON.stringify(fingerprint) );
+      console.log(`FINGERPRINT stored on device <<`);
+    } else {
+      console.log(`NO LOCAL STORAGE SUPPORT <<`);
+    }    
+    
+  })
+}
+
+fingerprint = localStorage.getItem("fingerprint");
+if ( fingerprint === null) {
+
+  // shiming https://developers.google.com/web/updates/2015/08/using-requestidlecallback#why_should_i_use_requestidlecallback
+  // https://www.npmjs.com/package/fingerprintjs2
+  //
+  if (window.requestIdleCallback) {          // check to see if requestIdleCallback available
+    requestIdleCallback(storeFingerprint);  // run storeFingerprint() when browser idle
+  } else {
+    setTimeout(storeFingerprint, 500)       // use timeout if requestIdleCallback NOT available
+  }
+} 
 
 
 // from server
 // var ts = dtk['dtk_rcp']['dt_date'];   // dtk timestamp on server
-
 // retrieve userUUID from local storage
 // TODO implement users / login / sessions 
 var userUUID = '014752da-b49d-4fb0-9f50-23bc90e44298';
 
 // on device
 var dbgOut = document.getElementById('debug_op');
-
-var dtkLocal;
-
-
-// TODO -break out into local storage utils - also in track_items.js
-// load dtk from local storage - if present
-if (typeof(Storage) !== "undefined") {
-  lastSavedFile = window.localStorage.getItem('lastSavedFile'); // key = 'lastSavedFile'
-  console.log(`LASTSAVED FILENAME = ${lastSavedFile} <<`);
-} else {
-  console.log(`NO LOCAL STORAGE SUPPORT <<`);
-}
 
 // JSON.parse(string, function)
     // create object from JSON string, func can be used for further conversion
@@ -37,33 +72,35 @@ if (typeof(Storage) !== "undefined") {
     // var obj = { "name":"John", "age":30, "city":"New York"};
     // var myJSON = JSON.stringify(obj);
 
-// load dtk object to ssm / 'disc' / nvm 
-lastSavedFile = window.localStorage.getItem('lastSavedFile');         // load name of last saved file
-dtkLocal = JSON.parse( window.localStorage.getItem(lastSavedFile) );  // key = content of lastSavedFile
+
+var dtkLocal;
+// TODO -break out into local storage utils - also in track_items.js
+// load dtk from local storage - if present
+if (typeof(Storage) !== "undefined") {
+  // load dtk object to ssm / 'disc' / nvm                      // key = 'lastSavedFile'
+  lastSavedFile = window.localStorage.getItem('lastSavedFile'); // load name of last saved file
+  dtkLocal = JSON.parse( window.localStorage.getItem(lastSavedFile) );  // key = content of lastSavedFile
   
+  console.log(`LASTSAVED FILENAME = ${lastSavedFile} <<`);
+} else {
+  console.log(`NO LOCAL STORAGE SUPPORT <<`);
+}
 
 console.log("Loaded DTK:");
 console.log(dtkLocal);
 console.log("<<\n<<\n<<\n<<");
 
 
-//console.log()
+// REMOVE - TODO - debug
 var dbgTxt = `Date timestamp:${dtkLocal['dtk_rcp']['dt_date']}`;
 var blank = "";
 dbgTxt += `<br>HRD UUID: ${userUUID}`;
 dbgTxt += `<br>DTK UUID: ${dtkLocal['dtk_user_info']['UUID']}`;
 dbgTxt += `<br>${lastSavedFile}`;
-dbgTxt += `<br>: ${blank}`;
+dbgTxt += `<br>: ${fingerprint}`;
 dbgTxt += `<br>: ${blank}`;
 
 dbgOut.innerHTML = dbgTxt;
-
-// no lib for initial simple id - but give theis a go fingerprintjs2
-// navigator.appName
-// navigator.appVersion
-// navigator.platform
-// navigator.userAgent
-fingerprint = {}
 
 
 console.log("POSTING DTK");
@@ -71,7 +108,7 @@ console.log("POSTING DTK");
 fetch( '/synch_n_route', {
   method: 'POST',                                             // method (default is GET)
   headers: {'Content-Type': 'application/json' },             // JSON
-  body: JSON.stringify( { 'user':userUUID, 'dtk':dtkLocal } )      // Payload        
+  body: JSON.stringify( { 'user':userUUID, 'dtk':dtkLocal, 'fp': fingerprint } )      // Payload        
 
 }).then( function(response) {
   
@@ -93,64 +130,6 @@ fetch( '/synch_n_route', {
 });
 
 
-
-
-
-// fingerprinting code
-
-var fingerprintReport = function () {
-  var d1 = new Date()
-  Fingerprint2.get( function(components) {
-    var murmur = Fingerprint2.x64hash128(components.map(function (pair) { return pair.value }).join(), 31);
-    var d2 = new Date();
-    var time = d2 - d1;
-
-    console.log(`==> FP: ${murmur}`);
-    console.log(`==> appName: ${navigator.appName}`);
-    console.log(`==> appVersion: ${navigator.appVersion}`);
-    console.log(`==> platform: ${navigator.platform}`);
-    console.log(`==> userAgent: ${navigator.userAgent}`);
-    console.log("==> time:", time);
-
-    //document.querySelector("#time").textContent = time
-    //document.querySelector("#fp").textContent = murmur
-    //var details = ""
-    //if(hasConsole) {
-      //console.log("time", time);
-      //console.log("fingerprint hash", murmur);
-    //}
-    //for (var index in components) {
-    //  var obj = components[index]
-    //  var line = obj.key + " = " + String(obj.value).substr(0, 100)
-    //  if (hasConsole) {
-    //    console.log(line)
-    //  }
-    //  details += line + "\n"
-    //}
-    //document.querySelector("#details").textContent = details
-  })
-}
-
-var cancelId;
-var cancelFunction;
-
-// see usage note in the README
-// 
-if (window.requestIdleCallback) {                     // check to see if requestIdleCallback available
-  cancelId = requestIdleCallback(fingerprintReport);  // run fingerprintReport() when browser idle
-  cancelFunction = cancelIdleCallback
-} else {
-  cancelId = setTimeout(fingerprintReport, 500)       // use timeout if requestIdleCallback NOT available
-  cancelFunction = clearTimeout
-}
-
-//document.querySelector("#btn").addEventListener("click", function () {
-//  if (cancelId) {
-//    cancelFunction(cancelId)
-//    cancelId = undefined
-//  }
-//  fingerprintReport()
-//})
 
 
 
