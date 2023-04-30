6dce796b994880c8423d7afd90935343603c512e
UnacceptableBehaviour <fernandez.simon@gmail.com>
2023-04-30 19:07:20 +0100

    Towards atomicLUT & exploded search

--- static/db_table_recipe_table_def.sql
+++ static/db_table_recipe_table_def.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS recipes (
   servings DECIMAL(9,2) DEFAULT NULL,
   density DECIMAL(9,2) DEFAULT NULL,
   serving_size DECIMAL(9,2) DEFAULT NULL,
-  atomic  BOOLEAN DEFAULT FALSE,
+  igdt_type SMALLINT DEFAULT -1,
   
   ingredients     VARCHAR(150) ARRAY,
   allergens       VARCHAR(150) ARRAY,
