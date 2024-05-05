e6c0300072ba080f49a074caef2b1e790f43805a
UnacceptableBehaviour <fernandez.simon@gmail.com>
2024-05-05 16:51:51 +0100

    Move archive off container

--- dockerClone-2-NAS-Setup.sh
+++ dockerClone-2-NAS-Setup.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env zsh
 
-
 # Make sure NAS mounted
-# cd /Volumes/docker/dtk-swarm-2/dtk_health/
-# git clone https://github.com/UnacceptableBehaviour/mysql_python
-# cd mysql_python
+# place this file in /Volumes/docker/dtk-swarm-2
+# run it to upgdate the mysql_python repo
+
+cd /Volumes/docker/dtk-swarm-2/dtk_health/
+
+# remove the old repo
+rm -rf mysql_python
+
+# clone new one in place
+git clone https://github.com/UnacceptableBehaviour/mysql_python
+
+cd mysql_python
 # chmod +x dockerClone-2-NAS-Setup.sh
-# ./dockerClone-2-NAS-Setup.sh
-# update the docker-compose.yml in Project from docker-compose-NAS.yml IF NEEDED
+
+# update the docker-compose.yml in Project on NAS from docker-compose-NAS.yml IF NEEDED
 
 # copy 'unmanaged' private config files over - from original repo
 # config files, ruby scripts
@@ -21,17 +29,12 @@ cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_user
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_users_devices_DB.json ./scratch/_docr_support
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/config_files.json ./scratch/_docr_support
 
-# Define the source and target directories
-src="/Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/archive/014752da-b49d-4fb0-9f50-23bc90e44298_carter_1703566856299_2023 12 26.json"
-target="./scratch/_docr_support/archive"
+# This is now stored off containe at: /Volumes/docker/dtk-swarm-2/dtk_health/archive
+# and tha is mapped in the container to
+# mysql_python/scratch/_docr_support/archive
+# diary entries are retained in the archive when the conatiner is rebuilt
 
-# Create the target directory if it doesn't exist
-mkdir -p "$target"
-
-# Copy the file
-cp "$src" "$target"
-
-
+# deprecated - REMOVE - Reverse proxy now does the SSL stuff
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/server.crt ./scratch
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/server.key ./scratch
 
