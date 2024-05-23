4bc7c9536a62e99bb2217414510a7a48b12df861
UnacceptableBehaviour <fernandez.simon@gmail.com>
2024-05-23 12:01:29 +0100

    Reconfigure mount point for persistence across rebuild

--- dockerClone-2-NAS-Setup.sh
+++ dockerClone-2-NAS-Setup.sh
@@ -20,23 +20,8 @@ cd mysql_python
 # copy 'unmanaged' private config files over - from original repo
 # config files, ruby scripts
 mkdir scratch
-mkdir scratch/_docr_support
 mkdir scratch/_ruby_scripts
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/nutrinfo.txt ./scratch/_docr_support
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/___LAB_RECIPE_SMALLEST_DTK_TEST.txt ./scratch/_docr_support
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_usersDTK_DB.json ./scratch/_docr_support
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_users_DB.json ./scratch/_docr_support
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_users_devices_DB.json ./scratch/_docr_support
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/config_files.json ./scratch/_docr_support
-
-# This is now stored off containe at: /Volumes/docker/dtk-swarm-2/dtk_health/archive
-# and tha is mapped in the container to
-# mysql_python/scratch/_docr_support/archive
-# diary entries are retained in the archive when the conatiner is rebuilt
-
-# deprecated - REMOVE - Reverse proxy now does the SSL stuff
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/server.crt ./scratch
-cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/server.key ./scratch
+#mkdir scratch/_docr_support    # automatically created in container when mounted
 
 # cp RUBY SCRIPTS OVER
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_ruby_scripts/ccm_nutridoc_web.rb ./scratch/_ruby_scripts
@@ -45,6 +30,29 @@ cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_ruby_scripts/foodla
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_ruby_scripts/foodlab_file_services.rb ./scratch/_ruby_scripts
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_ruby_scripts/foodlab_tools.rb ./scratch/_ruby_scripts
 cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_ruby_scripts/recipe.rb ./scratch/_ruby_scripts
+
+# deprecated - REMOVE - Reverse proxy now does the SSL stuff
+cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/server.crt ./scratch
+cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/server.key ./scratch
+
+# want _docr_support to persist across rebuilds - so map it to a volume in compose file
+cd ..
+cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/nutrinfo.txt ./_docr_support
+cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/___LAB_RECIPE_SMALLEST_DTK_TEST.txt ./_docr_support
+# z_usersDTK_DB.json contains diary entries so don't overwrite it if it exists!
+# diary will persis across rebuilds
+if [ ! -f ./_docr_support/z_usersDTK_DB.json ]; then
+    cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_usersDTK_DB.json ./_docr_support
+fi
+cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_users_DB.json ./_docr_support
+cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/z_users_devices_DB.json ./_docr_support
+cp /Users/simon/a_syllabus/lang/python/mysql_python/scratch/_docr_support/config_files.json ./_docr_support
+
+# This is now stored off container at: /Volumes/docker/dtk-swarm-2/dtk_health/archive
+# and tha is mapped in the container to
+# mysql_python/scratch/_docr_support/archive
+# diary entries are retained in the archive when the conatiner is rebuilt
+
 
 # that gets things ready for container manager to build dtk container
 
