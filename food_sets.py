fd4226d05351eb2086fd2ce13206a8d1eccb3ae1
UnacceptableBehaviour <fernandez.simon@gmail.com>
2024-06-04 23:03:12 +0100

    Add arg red shrimp

--- food_sets.py
+++ food_sets.py
@@ -1233,7 +1233,7 @@ crustaceans_subsets = {
     'lobster' : {'american lobster','rock lobster','spiny lobster','red lobster','canadian lobster'},
     'crayfish' : {'marron','koura'},
     'prawns' : {'tiger prawns','king prawns','cooked prawns','fresh prawns','fresh water prawns','tiger prawn',
-                'king prawn','cooked prawn','fresh prawn','fresh water prawn','argentinian prawns','argentinian prawn'}
+                'king prawn','cooked prawn','fresh prawn','fresh water prawn','argentinian prawns','argentinian prawn', 'argentine red shrimps', 'argentine red shrimp'}
 }
 def build_crustaceans_set():
     crustaceans = {'crustacean','crustaceans'}
